efc78a9bef8624947f325764305a428cee817799
Jonathan Disla <jon.disla@gmail.com>
2018-03-01 23:10:01 -0500

    update commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,47 @@
 var words = ["elephant", "dog", "cat", "mouse", "monkey", "giraffe", "tiger", "lion"];
-
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var newRandWord;
 var wins = 0;
 var loss = 0;
 var used = [];
 var left = [];
 
- document.onkeyup = function() {
-//     var string = String.fromCharCode(event.keyCode).toLowerCase();
+    document.onready
 
-//     document.getElementById("userOutput").innerHTML = string;
+ document.onkeypress = function(event) {
+    var string = String.fromCharCode(event.keyCode).toLowerCase();
+    //the document.getelement code is overwriting the words, find a way to store this
+    
+    // var storeWords = document.getElementById("userOutput").innerHTML = string;
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(string);
+    node.appendChild(textnode);
+    document.getElementById("userOutput").appendChild(node);
 
-//     var randomword = words[Math.floor(Math.random() * words.length)];
+}
+    
+    function randWords() {
+        newRandWord = words[Math.floor(Math.random() * words.length)];
+        var underScore = [];
+        for( var i = 0; i < newRandWord.length; i++){
+            underScore[i] = "_";
+             document.getElementById("ramword").innerHTML = underScore.join(" ");
+             console.log(newRandWord);
+            }
+        }
+         randWords();
 
-//     document.getElementById("ramword").innerHTML = randomword;
+    function wordZero(){
+        if (word=="elephant"){
+            alert("got it")
+        }
+    }
+    
+    
 
-   function consoleInside(arr) {
-       for (var i = 0; i< arr.length; i++){
-           console.log(arr[0]);
-       }
-       console.log("--------------");
-   }
-   consoleInside(words);
- }
-
-
+    //     ramword.addEventListener("keypress", function(){
+    //         alert("clicked");
+        
+    //     })
+    // }
+    // attempts();
